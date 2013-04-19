c26caa62d9ed3e8e8196f8e2ea75029fab651507
Luke Richards <luke.richards@emailcenteruk.com>
2013-04-19 16:24:34 +0100

    Moved comments from main class file to README.

--- MockFunction.php
+++ MockFunction.php
@@ -1,12 +1,5 @@
 <?php
 
-// https://github.com/zenovich/runkit
-//
-// https://github.com/tcz/phpunit-mockfunction/blob/master/PHPUnit/Extensions/MockFunction.php
-//
-// Extention Fail, it doesn't support override of internal functions
-// https://github.com/sebastianbergmann/php-test-helpers/blob/master/test_helpers.c
-
 /**
  * Mock Function
  */
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 phpunit-mockfunction
 ====================
+
+https://github.com/zenovich/runkit
+
+https://github.com/tcz/phpunit-mockfunction/blob/master/PHPUnit/Extensions/MockFunction.php
+
+Extention Fail, it doesn't support override of internal functions
+https://github.com/sebastianbergmann/php-test-helpers/blob/master/test_helpers.c
